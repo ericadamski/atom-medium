37e5448ecc1dc5107e71cec6fc7bc84f3b39bdd3
Eric Adamski <er.adamski@gmail.com>
2018-02-22 08:24:48 -0500

    Use react-markdown-menu

--- lib/components/main.jsx
+++ lib/components/main.jsx
@@ -1,30 +1,28 @@
-'use babel';
+"use babel";
 
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled from "styled-components";
 
-import User from './user.jsx';
-import Status from './status.jsx';
-import Menu from './menu.jsx';
+import User from "./user.jsx";
+import Status from "./status.jsx";
+import Menu from "./menu.jsx";
 
 const Container = styled.div`
-    max-height: 10rem;
-    padding: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+  max-height: 10rem;
+  padding: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const StatusContainer = styled.div``;
 
-export default class Main extends Component {
-    render() {
-        return (
-            <Container>
-                <Status />
-                <User />
-                <Menu />
-            </Container>
-        );
-    }
-}
+const Main = () => (
+  <Container>
+    <Status />
+    <User />
+    <Menu />
+  </Container>
+);
+
+export default Main;
